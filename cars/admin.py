a4e596686a704dd99b9cc185015d1a3e93e2d6dc
Denka88 <shadowfightdenka88@gmail.com>
2025-11-21 14:02:43 +0300

    intt

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
 from cars.models import Car
+from cars.models import CarsCharacteristics
 
 # Register your models here.
 admin.site.register(Car)
+admin.site.register(CarsCharacteristics)
