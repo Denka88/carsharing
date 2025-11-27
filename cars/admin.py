e4fe943f08dd4f8d644f97e231f23ea8eaf75c14
Denka88 <shadowfightdenka88@gmail.com>
2025-11-27 15:24:19 +0300

    cant add categories

--- cars/admin.py
+++ cars/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from cars.models import Car
+from cars.models import Car, CarsCategory
 from cars.models import CarsCharacteristics
 
 # Register your models here.
 admin.site.register(Car)
 admin.site.register(CarsCharacteristics)
+admin.site.register(CarsCategory)
