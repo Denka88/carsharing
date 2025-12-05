2753f314b57aa6e27288e82146fa7559a350463f
Denka88 <shadowfightdenka88@gmail.com>
2025-12-05 16:31:55 +0300

    form created

--- carsharing/urls.py
+++ carsharing/urls.py
@@ -20,7 +20,6 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from cars import views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model, login
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from users.forms import CreateUserCreationForm
 
 # Create your views here.
+
+User = get_user_model()
+
+def register_view(request):
+    if request.method == 'POST':
+        form = CreateUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('/')
+    else:
+        form = CreateUserCreationForm()
+    return render(request, 'register.html', {'form': form})
